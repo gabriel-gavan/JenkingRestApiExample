28c94822c84f82ed13a02190a6d1ce06eaafd088
Gabriel Gavan (Engineering) <gabriel.gavan@softelligence.net>
2023-01-25 18:01:45 +0200

    initial commit

---
+++ src/main/java/utils/BaseComponent2.java
@@ -0,0 +1,159 @@
+package utils;
+
+import org.hamcrest.core.AnyOf;
+import org.testng.annotations.BeforeClass;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+import static org.hamcrest.Matchers.anyOf;
+import static org.hamcrest.Matchers.is;
+import static io.restassured.RestAssured.given;
+
+import org.hamcrest.*;
+public class BaseComponent2 {
+	public static RequestSpecification requestSpec;
+	public static ResponseSpecification responseSpec;
+	public static RequestSpecification requestSpecToDoGet;
+	public static RequestSpecification requestSpecToDoDelete;
+	@BeforeClass
+	public static void createRequestSpecification() {
+		requestSpec = new RequestSpecBuilder().
+					//setBaseUri("https://keytrcrud.herokuapp.com/")
+					//.setBasePath("api/users/")
+				//setBaseUri("https://fakerestapi.azurewebsites.net/")
+				//.setBasePath("api/v1/Books/")
+				setBaseUri("https://keytodorestapi.herokuapp.com/")
+				.setBasePath("api/save/")
+					.setContentType(ContentType.JSON)
+					.addHeader("accept","application/json")
+					.build();
+	}
+	
+	@BeforeClass
+		public static void createRequestSpecificationTodoGet() {
+			requestSpecToDoGet = new RequestSpecBuilder().
+						
+					setBaseUri("https://keytodorestapi.herokuapp.com/")
+					.setBasePath("api/")
+						.setContentType(ContentType.JSON)
+						.addHeader("accept","application/json")
+						.build();
+	}
+	@BeforeClass
+	public static void createRequestSpecificationTodoDelete() {
+		requestSpecToDoDelete = new RequestSpecBuilder().
+					
+				setBaseUri("https://keytodorestapi.herokuapp.com/")
+				.setBasePath("api/delete")
+					.setContentType(ContentType.JSON)
+					.addHeader("accept","application/json")
+					.build();
+}
+	@BeforeClass
+	public static void createResponseSpecification() {
+		responseSpec =  new ResponseSpecBuilder()
+				.expectStatusCode(anyOf(is(200),is(201)))
+				.build();
+	}
+	public static Response doPostRequest(String body) {
+
+		Response result = 
+				given().
+					spec(requestSpec).
+					body(body).
+				when().
+					post().
+				then().
+					spec(responseSpec).
+					extract().response();
+
+		return result;
+
+	}
+	public static Response doPutRequest(String id, String body) {
+
+		Response result = 
+				given().
+					spec(requestSpec).
+					body(body).
+				when().
+					put(id).
+				then().
+					spec(responseSpec).
+					extract().response();
+
+		return result;
+
+	}
+	public static Response doGetAllRequest() {
+
+		Response result = 
+				given().
+					spec(requestSpec).
+				when().
+					get().
+				then().
+					spec(responseSpec).
+					extract().response();
+
+		return result;
+	}
+
+	public static Response doDeleteRequest(String id) {
+
+		Response result = 
+				given().
+				spec(requestSpec).
+				when().
+					delete(id).
+				then().
+				spec(responseSpec).
+					extract().response();
+
+		return result;
+	}
+	public static Response doGetOneRequest(String id) {
+
+		Response result = 
+				given().
+					spec(requestSpec).
+				when().
+					get(id).
+				then().
+					spec(responseSpec).
+					extract().response();
+
+		return result;
+	}
+	
+	public static Response doGetAllRequestToDo() {
+
+		Response result1 = 
+				given().
+					spec(requestSpecToDoGet).
+				when().
+					get().
+				then().
+					spec(responseSpec).
+					extract().response();
+
+		return result1;
+	}
+	public static Response doDeleteRequestToDo(String id) {
+
+		Response result = 
+				given().
+				spec(requestSpecToDoDelete).
+				when().
+					delete(id).
+				then().
+				spec(responseSpec).
+					extract().response();
+
+		return result;
+	}
+}
---
+++ src/main/java/utils/DataBuilder.java
@@ -0,0 +1,86 @@
+package utils;
+
+import org.json.simple.JSONObject;
+
+import com.github.javafaker.Faker;
+
+public class DataBuilder {
+	public static JSONObject buildUser() {
+		JSONObject bodyBuilder =  new JSONObject();
+		Faker faker = new Faker();
+		
+		String email = faker.internet().emailAddress();
+		bodyBuilder.put("name", faker.name().firstName());
+		bodyBuilder.put("email",email );
+		bodyBuilder.put("age", faker.number().numberBetween(5, 130));
+		bodyBuilder.put("gender", "m");
+		
+		
+		return bodyBuilder;
+	}
+	public static JSONObject buildToDo() {
+		JSONObject bodyToDO =  new JSONObject();
+		Faker faker = new Faker();
+	
+		bodyToDO.put("title", faker.book().title());
+		bodyToDO.put("body", "Request Body G" );
+		
+		return bodyToDO;
+	}
+	public static JSONObject buildTodos() {
+		
+			JSONObject totoBuilder =  new JSONObject();
+			Faker faker = new Faker();
+		
+			totoBuilder.put("title", faker.lordOfTheRings().character());
+			totoBuilder.put("body", faker.dune().saying() );
+			
+			return totoBuilder;
+	}
+	public static JSONObject buildToken() {
+				
+				JSONObject body =  new JSONObject();
+				
+			
+				body.put("username", "admin");
+				body.put("password", "password123" );
+				
+				return body;
+	}
+	public static JSONObject buildBooking() {
+		
+		/*{
+    "firstname" : "Jim",
+    "lastname" : "Brown",
+    "totalprice" : 111,
+    "depositpaid" : true,
+    "bookingdates" : {
+        "checkin" : "2018-01-01",
+        "checkout" : "2019-01-01"
+    },
+    "additionalneeds" : "Breakfast"
+}'*/
+		JSONObject booking = new JSONObject();
+		booking.put("firstname" , "John");
+		booking.put("lastname" ,"Doe");
+		booking.put("totalprice" , 111);
+		booking.put("depositpaid" , true);
+			JSONObject bookingDates = new JSONObject();
+			bookingDates.put("checkin" , "2023-01-01");
+			bookingDates.put("checkout" , "2023-01-11");
+		booking.put("bookingdates" , bookingDates);
+		booking.put("additionalneeds" , "Breakfast");
+		return booking;
+	}
+	public static JSONObject buildToken2() {
+		
+		JSONObject body =  new JSONObject();
+		
+	
+		body.put("user", "admin");
+		body.put("pass", "password123" );
+		
+		return body;
+}
+
+}
